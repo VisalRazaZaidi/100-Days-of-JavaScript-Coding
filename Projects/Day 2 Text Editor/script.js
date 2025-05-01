a12b1ee690644a5af74068bd3324159c44f08d24
visal raza <visalraza@gmail.com>
2025-05-01 18:22:14 +0500

    Refactor script.js to initialize additional UI elements and define font list

--- Projects/Day 2 Text Editor/script.js
+++ Projects/Day 2 Text Editor/script.js
@@ -1,1 +1,20 @@
-let optionsButtons = document.querySelectorAll(".option-button")
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll('adv-option-button');
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById('fontSize');
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButton = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButton = document.querySelectorAll("script");
+
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courior New",
+    "Cursive",
+];
