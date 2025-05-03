4112197cd309138708db783edf5b7cf368ea1c32
visal raza <visalraza@gmail.com>
2025-05-03 17:16:20 +0500

    updated PRoject 4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projects = [
     { day: 1, name: "Pixel Art Generator", demoLink: "/Projects/Day 1 Pixel Art generator/", githubLink: "https://github.com/VisalRazaZaidi/100-Days-of-JavaScript-Coding/tree/bfb40252d3108b57e5f8e85fd3b0a6ed2f6636fe/Projects/Day%201%20Pixel%20Art%20generator" },
     { day: 2, name: "Text Editor ", demoLink: "/Projects/Day 2 Text Editor/", githubLink: "https://github.com/VisalRazaZaidi/100-Days-of-JavaScript-Coding/tree/main/Projects/Day%202%20Text%20Editor" },
     { day: 3, name: "Weather App", demoLink: "https://minimal-weather-app-ui-design.vercel.app/", githubLink: "https://github.com/VisalRazaZaidi/MinimalWeatherAppUiDesign?tab=readme-ov-file" },
-    { day: 4, name: "Password Generator", demoLink: "", githubLink: "" },
+    { day: 4, name: "Password Generator", demoLink: "/Projects/Day 4 Password generator/", githubLink: "" },
     { day: 5, name: "BMI Calulator", demoLink: "/Projects/Day 5 BMI Calulator/", githubLink: "https://github.com/VisalRazaZaidi/100-Days-of-JavaScript-Coding/tree/main/Projects/Day%205%20BMI%20Calulator" },
     { day: 6, name: "", demoLink: "", githubLink: "" },
     { day: 7, name: "", demoLink: "", githubLink: "" },
